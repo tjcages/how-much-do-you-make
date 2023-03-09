651e3477ff2e2d5a94d96abcda56d53585262182
tylerj <tylerjcagle@gmail.com>
2023-03-09 12:20:44 -0500

    update preview image

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -5,7 +5,7 @@ const Meta = (props) => {
   const description =
     props.description ??
     "An inside look at the personal finances of young professionals.";
-  const image = props.image ?? "/preview.jpg";
+  const image = props.image ?? "https://howmuchdoyoumake.xyz/preview.jpg";
   const url = props.url ?? "https://howmuchdoyoumake.xyz";
 
   return (
